0377ec44d57f51dd1c8a9674873a1292c8abe7eb
Bennett Upfield <bjupfield@gmail.com>
2023-11-09 22:53:24 -0600

    not much today, added outline to heartbeat func, removed visibility of objects in darkness, and createad an easily applicable scene for the heartbeat, plans currently are to make the scene follow whatever is labeled 'player' in the node its in and copy 'players' camera movement

--- Scripts/OverlayScript.gd
+++ Scripts/OverlayScript.gd
@@ -1,21 +1,21 @@
 extends CanvasLayer
+@export_range(1, 10) var objectLayer:int = 2;
 
 var childMat:ShaderMaterial;
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	childMat = self.get_child(0).material
-	#self.get_child(0).get_material().set_shader_parameter("objectMap",self.get_parent().get_child(6).get_viewport())
+	self.get_child(1).rLayer = objectLayer
 	self.visible = true;
-	#ib.set_shader_param(self.get_parent().get_child(6).get_viewport())
-	# = self.get_parent().get_child(6).get_viewport()
+
 	
-	#get_tree().get_root().get_viewport().set_canvas_cull_mask_bit(1, false);
+	get_tree().get_root().get_viewport().set_canvas_cull_mask_bit(objectLayer - 1, false);
 	
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	print(self.get_parent().heartBeat)
+	#print(self.get_parent().heartBeat)
 	childMat.set_shader_parameter("heartbeat", self.get_parent().heartBeat)
 	pass
